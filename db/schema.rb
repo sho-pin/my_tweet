fa23ed98bf8ea270d6f64c26893a257266354ebe
shota <f.shotale@gmail.com>
2021-07-06 18:27:26 +0900

    ログイン機能実装。レッスンⅧ終了。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_082622) do
+ActiveRecord::Schema.define(version: 2021_07_05_084533) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_07_03_082622) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_name"
+    t.string "password"
   end
 
 end
